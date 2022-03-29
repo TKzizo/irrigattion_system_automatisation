cbf59e14c317095f3c3a77213d7e6818d1297eab
fas <ia_takouche@esi.dz>
2022-03-30 00:22:45 +0100

    added the api

---
+++ common/crud.py
@@ -0,0 +1,31 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        
+    except Error as e:
+        print(e)
+    finally:
+        return conn
+
+
+
+def close_connection(conn):
+    conn.close()
+
+
+def insert_sensors_values(conn,values):
+    sql = """ INSERT INTO arduino_sensors (id,date,air,water,soil) 
+              VALUES(?,datetime("now"),?,?,?)
+          """
+
+    cur = conn.cursor()
+    cur.execute(sql,values)
+    conn.commit()
+
+    return cur.lastrowid
+
